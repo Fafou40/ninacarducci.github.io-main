18e41b357384b7a809906b2a794a1c2c8fcc2b33
Fafou40 <125992725+Fafou40@users.noreply.github.com>
2023-12-07 22:03:30 +0100

    min css

--- purifycss.sh
+++ purifycss.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-
-purifycss assets/bootstrap/bootstrap.min.css index.html assets/maugallery.js assets/scripts.js assets/bootstrap/bootstrap.bundle.min.js --min --info --out assets/bootstrap.purified.min.css
-purifycss assets/style.min.css index.html assets/maugallery.js assets/scripts.js assets/bootstrap/bootstrap.bundle.min.js --min --info --out assets/style.purified.min.css
+purifycss assets/bootstrap/bootstrap.css index.html assets/maugallery.js assets/scripts.js --min -r --info --out assets/bootstrap/bootstrap.min.css
+purifycss assets/style.css index.html assets/maugallery.js assets/scripts.js assets/bootstrap/bootstrap.bundle.min.js --min -r --info --out assets/style.min.css
